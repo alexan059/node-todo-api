3c8c8a7e60e4fd391715919f111f1fe4b4069449
Alexander <accounts.alexan059@gmail.com>
2017-04-07 18:38:28 +0200

    Set up environment databases

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 let mongoose = require('mongoose');
 
-const db = {
-    mlab: "mongodb://admin:abcd1234@ds033607.mlab.com:33607/node-todo-app-186458231",
-    local: "mongodb://localhost:27017/TodoApp"
-};
-
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PORT ? db.mlab : db.local);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
