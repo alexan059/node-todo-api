11fc3e331eab01767b736c7e2f430a41b1345d6d
Alexander <accounts.alexan059@gmail.com>
2017-04-07 14:20:43 +0200

    Corrected MongoDB URL

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
-const dbUrl = "mongodb://>admin:abcd1234@ds033607.mlab.com:33607/node-todo-app-186458231";
+const dbUrl = "mongodb://admin:abcd1234@ds033607.mlab.com:33607/node-todo-app-186458231";
 
 mongoose.Promise = global.Promise;
 mongoose.connect((process.env.port) ? dbUrl : 'mongodb://localhost:27017/TodoApp');
