749ee213a184c093d46339efce4ee1cda2755e4c
Alexander <accounts.alexan059@gmail.com>
2017-04-07 13:55:00 +0200

    Add test GET /todos/:id

--- server/server.test.js
+++ server/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const {app} = require('./server');
 const {Todo} = require('./models/todo');
 
 const todos = [{
+    _id: ObjectId(),
     text: 'First todo test'
 }, {
+    _id: ObjectId(),
     text: 'Second todo test'
 }];
 
@@ -71,5 +74,35 @@ describe('GET /todos', () => {
             .end(done);
 
     });
+
+});
+
+describe('GET /todos/:id', () => {
+
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect((res.body.todo.text)).toBe(todos[0].text)
+            })
+            .end(done);
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        let hexId = ObjectId().toHexString();
+
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    })
 
 });
