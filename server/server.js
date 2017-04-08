7905d0cd0cf034a01e909599b2e97d7c322d66e4
Alexander <accounts.alexan059@gmail.com>
2017-04-08 02:25:45 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -123,11 +123,24 @@ app.post('/users', (req, res) => {
     })
 });
 
-
-
 // GET users/me
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+// POST users/login {email, password}
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        }).catch((err) => {
+            res.status(400).send(err);
+        })
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
 });
 
 app.listen(port, () => {
